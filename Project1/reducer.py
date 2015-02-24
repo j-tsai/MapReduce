c4cb35520f56b4bb3fc19d0dc4a6ea7b26001697
jetsai <jen.cy.tsai@gmail.com>
2015-02-24 14:33:47 -0800

    Update and rename Project1 to Project1/reducer.py

---
+++ Project1/reducer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+
+current_word = None
+current_count = 0
+word = None
+maxWord = None
+maxCount = 0
+
+for line in sys.stdin:
+	word, count = line.strip().split("\t",1)
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+	if current_word == word:
+		current_count += count
+	else:
+# print counts of each path 
+#		if current_word:
+#			print '%s\t%s' % (current_word, current_count)
+		if current_count > maxCount:
+			maxCount = current_count
+			maxWord = current_word
+		elif current_count == maxCount:
+			maxWord += '\n' + current_word
+		current_count = count
+		current_word = word
+
+# print last line if necessary
+#if current_word == word:
+#	print '%s\t%s' % (current_word, current_count)
+print '%s\t%s' % (maxCount, maxWord)		
