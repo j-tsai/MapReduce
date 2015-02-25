bef2c6660d853be394f1f2745eefc814e631583c
jetsai <jen.cy.tsai@gmail.com>
2015-02-25 15:22:47 -0800

    Update and rename word_counts to word_counts/reducer.py

---
+++ word_counts/reducer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+
+fantastic = 0
+fantastically = []
+for line in sys.stdin:
+    data = line.split("\t")
+    if len(data) != 2:
+        continue
+
+    node = data[0]
+    #body0 = [i.lower() for i in ' '.join(data[1])]
+    body = data[1].split('|')
+    fantastic += body.count('fantastic')
+    if 'fantastically' in body:
+        fantastically.append(int(node))
+print "num 'fantastic':", "\t", fantastic
+print "list of csv nodes the word 'fantastically' found:", "\t", ','.join(map(str,sorted(fantastically)))
